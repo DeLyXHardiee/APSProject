1c59c6278c46584dccedc6b5f821278b975a7f98
Anders Arvesen <aarv@itu.dk>
2023-05-21 16:32:42 +0200

    blabla

--- WeWorkInTheDark/data/generate_random.py
+++ WeWorkInTheDark/data/generate_random.py
@@ -1,14 +1,14 @@
 import random
 import sys
 
-random.seed(int(sys.argv[-1])) # fix seed of random generator to last argument
+#random.seed(int(sys.argv[-1])) # fix seed of random generator to last argument
 
 #print(random.randrange(0, pow(10, 18)))
 
 # Generate random input
-X = random.randint(1, 1000000)
-Y = random.randint(1, 1000000)
-N = random.randint(1, min(X * Y, 100))
+X = random.randint(1000, 1000)
+Y = random.randint(1000, 1000)
+N = random.randint(100000, 100000)
 guards = []
 for _ in range(N):
     x = random.randint(0, X - 1)
@@ -17,15 +17,15 @@ for _ in range(N):
     update = f"{x} {y} {num_guards}"
     guards.append(update)
 
-M = random.randint(1, 1000000)
+M = random.randint(100000, 100000)
 queries = []
 for _ in range(M):
-    query_type = random.choice(["kill", "eaglevision", "reinforcements"])
-    if query_type == "kill":
+    query_type = random.choice(["e", "r", "k"])
+    if query_type == "k":
         x = random.randint(0, X - 1)
         y = random.randint(0, Y - 1)
         query = f"{query_type} {x} {y}"
-    elif query_type == "eaglevision":
+    elif query_type == "e":
         x = random.randint(0, X - 1)
         y = random.randint(0, Y - 1)
         r = random.randint(1, max(X, Y))
@@ -44,3 +44,12 @@ for guard in guards:
 print(M)
 for query in queries:
     print(query)
+
+# filename = "input.txt"  # Change the filename as desired
+# with open(filename, "w") as file:
+#     file.write(f"{X} {Y} {N}\n")
+#     for guard in guards:
+#         file.write(f"{guard}\n")
+#     file.write(f"{M}\n")
+#     for query in queries:
+#         file.write(f"{query}\n")
